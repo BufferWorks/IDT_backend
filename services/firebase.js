09ad19157c9197cc0c3ed01ae84d98fe52232e68
BlackHat86 <suryansh.c05@gmail.com>
2026-01-01 19:31:41 +0530

    changed firebase.js code

--- services/firebase.js
+++ services/firebase.js
@@ -2,13 +2,13 @@ const { initializeApp } = require("firebase/app");
 const { getAuth } = require("firebase/auth");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBfC_rgf2zBdUImT8lAdapuDCPPNGuTpQ0",
-    authDomain: "contestapp1204.firebaseapp.com",
-    projectId: "contestapp1204",
-    storageBucket: "contestapp1204.firebasestorage.app",
-    messagingSenderId: "207824794969",
-    appId: "1:207824794969:web:5abab97755ffc985a58848",
-    measurementId: "G-9DKWLM5SGP"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
