3705359031218296873080921744b11bca51d5ac
BlackHat86 <suryansh.c05@gmail.com>
2026-01-01 18:51:32 +0530

    Use Firebase Admin via env variables

--- services/adminFirebase.js
+++ services/adminFirebase.js
@@ -1,10 +1,19 @@
-// services/adminFirebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("../config/firebaseServiceAccountKey.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      type: "service_account",
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      clientId: process.env.FIREBASE_CLIENT_ID,
+      authUri: process.env.FIREBASE_AUTH_URI,
+      tokenUri: process.env.FIREBASE_TOKEN_URI,
+      authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+      clientX509CertUrl: process.env.FIREBASE_CLIENT_CERT_URL,
+    }),
   });
 }
 
